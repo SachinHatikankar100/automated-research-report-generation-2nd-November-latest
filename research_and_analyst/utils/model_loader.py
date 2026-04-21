37f08e1f3c4aab43b989826abd619ea1067a0e7a
SachinHatikankar100 <shatikankan@gmail.com>
2026-04-21 09:02:23 +0530

    updated model file

--- research_and_analyst/utils/model_loader.py
+++ research_and_analyst/utils/model_loader.py
@@ -7,7 +7,7 @@ from research_and_analyst.utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-from logger import GLOBAL_LOGGER as log
+from research_and_analyst.logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ResearchAnalystException
 
 
