54636d7c3b64b3f698390159aaf0991c0740e56d
SachinHatikankar100 <shatikankan@gmail.com>
2026-04-21 09:25:55 +0530

    updated exception file

--- research_and_analyst/utils/config_loader.py
+++ research_and_analyst/utils/config_loader.py
@@ -3,7 +3,7 @@ import yaml
 import json
 from pathlib import Path
 from research_and_analyst.logger import GLOBAL_LOGGER as log
-from exception.custom_exception import ResearchAnalystException
+from research_and_analyst.exception.custom_exception import ResearchAnalystException
 
 
 def _project_root() -> Path:
