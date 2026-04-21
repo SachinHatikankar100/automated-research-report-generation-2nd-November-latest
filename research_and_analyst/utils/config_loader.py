6d627684057a29f14b50135b2b06f4ea16248142
SachinHatikankar100 <shatikankan@gmail.com>
2026-04-21 09:14:51 +0530

    updated config loader code

--- research_and_analyst/utils/config_loader.py
+++ research_and_analyst/utils/config_loader.py
@@ -2,7 +2,7 @@ import os
 import yaml
 import json
 from pathlib import Path
-from logger import GLOBAL_LOGGER as log
+from research_and_analyst.logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ResearchAnalystException
 
 
